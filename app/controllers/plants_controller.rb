2cdd945bba9b70cb243458fb074a3fa4435d635d
alexandra <alexandraschurman@gmail.com>
2022-03-18 10:51:54 -0700

    commit tree 7ca566ec3059c60bed829bd7eee45aee74bcda4f

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :rescue_from_response
 
   # GET /plants
   def index
@@ -8,7 +9,8 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
+    # plant = Plant.find(params[:id])
     render json: plant
   end
 
@@ -18,9 +20,50 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+
+  #  # PATCH /plants/:id
+  #  def update
+  #   plant = Plant.find_by(id: params[:id])
+  #   plant.update(plant_params) #I think it has to do with the params { is_in_stock: false }
+  #   render json: plant
+  # end
+
+  # # DELETE /plants/:id
+  # def destroy
+  #   plant = Plant.find_by(id: params[:id])
+  #   plant.destroy
+  #   head :no_content
+  # end
+
+  # ALI SOLUTION BELOW----
+  # PATCH /plants/:id
+
+  def change 
+    plant = find_plant
+      plant.update(plant_params)
+      render json: plant
+  end
+
+  # DELETE /plants/:id
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
+  def rescue_from_response
+    render json: plant, status: :not_found
+  end
+
 end
